aabec17c2487bf7f8c9712a2ee0b283e053738ad
Dan <dversabeau@gmail.com>
2022-05-13 12:34:01 +0200

    vendredi 13 midi

--- src/components/Prevision.js
+++ src/components/Prevision.js
@@ -1,3 +1,4 @@
+import { tab } from '@testing-library/user-event/dist/tab';
 import { useEffect, useState } from 'react';
 import './Prevision.css'
 import PrevisionCard from './PrevisonCard';
@@ -5,27 +6,53 @@ import PrevisionCard from './PrevisonCard';
 function Prevision(props) {
 
     const { list } = props;
-    // const listDateExceptToday = [];
-    const [listDateExceptToday, setListDateExceptToday] = useState([])
     const current = new Date();
     const today = current.getFullYear() + '-' + (current.getMonth.length > 2 ? (current.getMonth() + 1) : ('0' + (current.getMonth() + 1))) + '-' + current.getDate()
+    let t_date = '';
+    const t_array = [];
+    const t_arrayGlobal = [];
 
+    console.log('list', list)
+
+    const prevCond = () => {
+
+        if (list && list.length > 0) {
+            
+            list.map((item) => {
+        
+                // Si t_date n'existe pas il prend la date du premier item
+                if (t_date === '') t_date = item.dt_txt.substring(0, 10);
+                
+                // Si t_date et la date de l'item sont equivalente alors on push l'item
+                // dans t_array
+                if (t_date.substring(0, 10) === item.dt_txt.substring(0, 10)) {
+        
+                    t_array.push(item)
+                    console.log('t_array', t_array)
+
+                // Si la t_date est différente d'item alors on change t_date pour la date d'item
+                // puis on push une copie du tableau temporaire dans le tableau general et 
+                // on réinitialise t_array
+                } else if (t_date.substring(0, 10) !== item.dt_txt.substring(0, 10)) {
+                    t_date = item.dt_txt.substring(0, 10);
+                    t_arrayGlobal.push(t_array.slice());
+                    t_array.length = 0;
+        
+                }
+        
+            })
+
+        } 
+        console.log('t_arrayGlobal', t_arrayGlobal)
+    }
+
+    useEffect(() => {
+        prevCond()
+    }, []);
 
 
     return (
         <div>
-            {
-                list && list.map((item, index) => {
-                    if (item.dt_txt.substring(0, 10) !== today) {
-                        
-                        return (
-                            <PrevisionCard key={index} weather={item}/>
-                        )
-
-                    }
-
-                })
-            }
 
         </div>
     )
--- src/components/PrevisonCard.js
+++ src/components/PrevisonCard.js
@@ -4,12 +4,11 @@ function PrevisionCard(props) {
 
   const { weather } = props;
   console.log('weather card', weather)
-  console.log('weather description', weather.weather[0].description)
 
 
   return (
     <div className='flex column align-center prevision-card-body'>
-      <p>{weather.weather[0].description}</p>
+      {/* <p>{weather.weather[0].description}</p> */}
     </div>
   );
 
