c734b27a14e09385c2aae3d059a1b28ac7231a81
dversabeau <dversabeau@gmail.com>
2022-05-23 09:18:42 +0200

    cordova ok

--- src/index.js
+++ src/index.js
@@ -5,14 +5,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
+const startApp = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    // <React.StrictMode>
     <App />
-  // </React.StrictMode>
-);
+    // </React.StrictMode>
+  );
+}
+
+if (window.cordova) {
+  document.addEventListener('deviceready', startApp, false);
+  
+  reportWebVitals();
+} else {
+  startApp();
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
